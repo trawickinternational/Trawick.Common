3a462292922d818a9fefbcfc0cc531b071be7632
sdobbins76 <scott@dobbinsweb.com>
2020-01-29 09:29:46 -0600

    asdfasdf

--- Trawick.Common/Extensions/StringExtensions.cs
+++ Trawick.Common/Extensions/StringExtensions.cs
@@ -373,6 +373,51 @@ namespace Trawick.Common.Extensions
 			}
 		}
 
+        public static bool IsValidEmail(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return false;
 
-	}
+            try
+            {
+                // Normalize the domain
+                value = Regex.Replace(value, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    var domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(value,
+                    @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                    @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+
+    
 }
