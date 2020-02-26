35d16a63ead9602c19ba37143f8515d01b875d85
sdobbins76 <scott@dobbinsweb.com>
2020-02-26 14:12:02 -0600

    asdfasdf

--- Trawick.Common/Extensions/ImageExtensions.cs
+++ Trawick.Common/Extensions/ImageExtensions.cs
@@ -27,12 +27,27 @@ namespace Trawick.Common.Extensions
 		{
 			using (var ms = new MemoryStream())
 			{
-				image.Save(ms, image.RawFormat);
+				image.Save(ms, ImageFormat.Bmp);
 				return ms.ToArray();
 			}
 		}
 
-		public static string ToDataURL(this Image image)
+
+        public static byte[] ToByteArray(this Stream input)
+{
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public static string ToDataURL(this Image image)
 		{
 			string dataStr = Convert.ToBase64String(image.ToByteArray());
 			return string.Format("data:{0};base64,{1}", image.GetMimeType(), dataStr);
